fdead273db04a09946ab22e5d1df77385cf96e3f
Justin Lewis <justin@littlebeetlelabs.com>
2019-04-04 17:54:50 -0500

    Added tap to reveal functionality

--- Fifty States/MemoryGameVC.swift
+++ Fifty States/MemoryGameVC.swift
@@ -12,45 +12,55 @@ class MemoryGameVC: UIViewController {
 
     @IBOutlet var cardButtons: [UIButton]!
     
-    var cardCount = 0
     var currentStates = [Int]()
     
     //MARK:- Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        cardCount = cardButtons.count
-        
+        setButtonTags()
         dealNewCards(self)
+        stopPeeking(self)
     }
+    
     
     //MARK:- Actions
     @IBAction func flipCard(_ sender: UIButton) {
-        let tempImage = UIImage(named: "Texas")
-        var thisButton: UIButton {
-            for button in cardButtons {
-                if button == sender {
-                    return button
-                }
-            }
-            return cardButtons[0]
-        }
+        let thisStateIndex = currentStates[sender.tag]
+        let thisStateName = stateDetails[thisStateIndex].name  // get the name of each state
+        let tempImage = UIImage(named: thisStateName)
         
-        thisButton.setBackgroundImage(tempImage, for: .normal)
+        sender.setBackgroundImage(tempImage, for: .normal)
         
+//        var thisButton: UIButton {
+//            for button in cardButtons {
+//                if button == sender {
+//                    return button
+//                }
+//            }
+//            return cardButtons[0]
+//        }
+//        thisButton.setBackgroundImage(tempImage, for: .normal)
     }
 
     @IBAction func peekAtCards(_ sender: Any) {
-        showAllTheCards()
+        for (cardNumber, stateIndex) in currentStates.enumerated() { //loop through the chosen states
+            let thisStateName = stateDetails[stateIndex].name  // get the name of each state
+            let thisStateImage = UIImage(named: thisStateName)  //get the flag image for that name
+            cardButtons[cardNumber].setBackgroundImage(thisStateImage, for: .normal) //put the flag on the appropriate button
+        }
     }
     
     @IBAction func stopPeeking(_ sender: Any) {
-        hideAllTheCards()
+        let cardBackImage = UIImage(named: "USAcardback")
+        for card in cardButtons {
+            card.setBackgroundImage(cardBackImage, for: .normal)
+        }
     }
     
     
     @IBAction func dealNewCards(_ sender: Any) {
         currentStates = []
-        for _ in 0..<cardCount {
+        for _ in 0..<cardButtons.count {
             var randStateID = 0
             repeat{
                 randStateID = Int.random(in: 0...49)
@@ -60,19 +70,13 @@ class MemoryGameVC: UIViewController {
     }
     
     //MARK:- My Methods
-    func showAllTheCards() {
-        for (cardNumber, stateIndex) in currentStates.enumerated() { //loop through the chosen states
-            let thisStateName = stateDetails[stateIndex].name  // get the name of each state
-            let thisStateImage = UIImage(named: thisStateName)  //get the flag image for that name
-            cardButtons[cardNumber].setBackgroundImage(thisStateImage, for: .normal) //put the flag on the appropriate button
+    func setButtonTags() {
+        for (index, button) in cardButtons.enumerated() {
+            button.tag = index
         }
     }
     
-    func hideAllTheCards() {
-        for card in cardButtons {
-            card.setBackgroundImage(nil, for: .normal)
-        }
-    }
+    
     
 
 }
